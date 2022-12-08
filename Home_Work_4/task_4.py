f05f8b75eb461db7f6bf87a6174ed146da967b0c
mrFTO <ntoon@ya.ru>
2022-12-08 17:58:51 +0300

    Done task_4

--- Home_Work_4/task_4.py
+++ Home_Work_4/task_4.py
@@ -3,29 +3,31 @@
 # #     k=2 => 2*x*2 + 4*x + 5 = 0 или x2 + 5 = 0 или 10*x*2 = 0
 # #     k=3 => 2*x*3 + 4*x*2 + 4*x + 5 = 0
 
-# import random
+import random # Импортируем функцию случайной генерации
 
+# Создаем функцию для формирования списка коэффициентов многочлена 
+def generator(k):
+    for i in range(k+1):
+        x = random.randint(0, 100)
+        if k > 1:
+            if x > 1:
+                print(f'{x}x^{k} +', end=' ')
+            elif x == 1:
+                print(f'x^{k} +', end=' ')
+        elif k == 1:
+            if x > 1:
+                print(f'{x}x +', end=' ')
+            elif x == 1:
+                print(f'x +', end=' ')
+        elif k == 0:
+            if x != 0:
+                print(f'{x} = 0')
+            else:
+                print('= 0')
+        x += 1
+        k -= 1
 
-# def expr(k):
-#     for i in range(k+1):
-#         x = random.randint(0, 100)
-#         if k > 1:
-#             if x > 1:
-#                 print(f'{x}x^{k} +', end=' ')
-#             elif x == 1:
-#                 print(f'x^{k} +', end=' ')
-#         elif k == 1:
-#             if x > 1:
-#                 print(f'{x}x +', end=' ')
-#             elif x == 1:
-#                 print(f'x +', end=' ')
-#         elif k == 0:
-#             if x != 0:
-#                 print(f'{x} = 0')
-#             else:
-#                 print('= 0')
-#         x += 1
-#         k -= 1
-
-# k = int(input('Введите степень: '))
-# expr(k)
+# запрашиваем у пользователя значение для натуральной степени и помещаем его в числовом формате в переменную k
+k = int(input('Please enter a natural exponent: '))
+# запускаем функцию для введенной степени.
+generator(k)
